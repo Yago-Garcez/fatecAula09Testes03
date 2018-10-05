46b93080695426b00910f2cc711a86352bb73588
Yago-Garcez <32469486+Yago-Garcez@users.noreply.github.com>
2018-10-04 21:56:38 -0300

    First Commit - Aula 04/10/2018

--- sce/src/main/java/com/fatec/sce/model/FabricaDeConexoes.java
+++ sce/src/main/java/com/fatec/sce/model/FabricaDeConexoes.java
@@ -1,16 +1,19 @@
 package com.fatec.sce.model;
 
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.exceptions.jdbc4.CommunicationsException;
+import com.mysql.cj.jdbc.exceptions.CommunicationsException;
+
+//import com.mysql.jdbc.Connection;
+//import com.mysql.jdbc.exceptions.jdbc4.CommunicationsException;
 
 public class FabricaDeConexoes {
-	String url = "jdbc:mysql://localhost:3306/biblioteca";
-	String driver = "com.mysql.jdbc.Driver";
+	String url = "jdbc:mysql://localhost:3306/biblioteca?useTimezone=true&serverTimezone=UTC&useSSL=false";
+	String driver = "com.mysql.cj.jdbc.Driver";
 	String usuario = "root";
-	String senha = "";
+	String senha = "ysg140600";
 
 	public FabricaDeConexoes(ConfiguraDB configura) {
 		this.url = configura.getUrl();
